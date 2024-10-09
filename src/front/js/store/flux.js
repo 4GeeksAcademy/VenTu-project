52eec5045d98908536cd98f1f8c36d1f4018908a
Alex Riascos <160887203+X4Nn3R@users.noreply.github.com>
2024-10-09 22:30:35 +0000

    Se conecto el metodo POST con el front

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -32,7 +32,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					body: JSON.stringify({
 						email: email,
 						username: fullName,
-						password_hash: password
+						password: password
 					})
 				});
 				const data = await resp.json();
@@ -51,14 +51,14 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 
 			getMessage: async () => {
-				try{
+				try {
 					// fetching data from the backend
 					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
 					return data;
-				}catch(error){
+				} catch (error) {
 					console.log("Error loading message from backend", error)
 				}
 			},
