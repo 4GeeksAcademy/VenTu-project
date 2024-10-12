bb0c5d55d802b893aa249206d66c97471b2d2f85
Gustavo L <95352419+G-code7@users.noreply.github.com>
2024-10-12 03:40:28 +0000

    backend actualizado, integración de firebase y manejo de iamgenes.

---
+++ src/api/firebase.py
@@ -0,0 +1,15 @@
+import pyrebase
+import os
+
+config = {
+    "apiKey": os.getenv("API_KEY"),
+    "authDomain": os.getenv("AUTH_DOMAIN"),
+    "projectId": os.getenv("PROJECT_ID"),
+    "storageBucket": os.getenv("STORAGE_BUCKET"),
+    "messagingSenderId": os.getenv("MESSAGING_SENDER_ID"),
+    "appId": os.getenv("APP_ID"),
+    "databaseURL": os.getenv("DATABASE_URL")
+}
+
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
