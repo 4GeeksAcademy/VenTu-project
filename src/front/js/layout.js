74aa61760f70062eb88b37b41be18e7f79bc8cc4
Yojan S. Hungría <ys.hungria@uniandes.edu.co>
2024-10-16 20:11:13 +0000

    Changes on b2b

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -25,7 +25,7 @@ const Layout = () => {
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
-                    <Navbar />
+                    <Navbar /> 
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
