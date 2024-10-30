3fa6cbafaa190c3da7b943083783a85923ead228
Alex Riascos <160887203+X4Nn3R@users.noreply.github.com>
2024-10-30 23:23:44 +0000

    algunos cambios de estilo en navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -17,6 +17,7 @@ export const Navbar = () => {
     const handleLogout = () => {
         actions.logout();
         navigate("/");
+        window.location.reload();
     };
 
     const handleLogin = async () => {
@@ -30,9 +31,11 @@ export const Navbar = () => {
                 }
                 if (store.user.role === 'provider') {
                     navigate("/");
+                    window.location.reload();
                 } else {
 
                     navigate("/");
+                    window.location.reload();
 
                 }
             } else {
@@ -48,134 +51,138 @@ export const Navbar = () => {
         <nav className="navbar navbar-light p-0" style={{ backgroundColor: '#00B4E7' }}>
             <Toaster /> {/* Renderiza los mensajes toast */}
 
-            <div className="container d-flex justify-content-start">
-                <Link to="/" className="navbar-brand text-white">
-                    <img src={logo} alt="Ventu Logo" style={{ width: '120px', height: 'auto' }} />
-                </Link>
-                <Link to="/" className="btn">Inicio</Link>
-                <Link to="/tourplans" className="btn">Planes turisticos</Link>
-                <Link to="/about" className="btn">Sobre Nosotros</Link>
-                <Link to="/favorites" className="btn ">Favoritos</Link>
-            </div>
-            <div className="dropdown">
-                <button
-                    className="btn dropdown-toggle"
-                    type="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                >
-                    {store.user ? `${store.user.username}` : "Iniciar / Registrarse"}
-                </button>
-                {!store.token ? (
-                    <ul className="dropdown-menu">
-                        <li>
-                            <button
-                                className="dropdown-item"
-                                data-bs-toggle="modal"
-                                data-bs-target="#exampleModal"
-                            >
-                                Iniciar sesión
-                            </button>
-                        </li>
-                        <li>
-                            <Link to="/register" className="btn btn-outline-light dropdown-item">
-                                Registrarse
-                            </Link>
-                        </li>
-                    </ul>
-                ) : (
-                    <ul className="dropdown-menu">
-                        <li>
-                            <button
-                                className="dropdown-item"
-                                onClick={handleLogout}
-                            >
-                                Cerrar sesión
-                            </button>
-                        </li>
-                        <li>
-                            <Link to="/favorites" className="btn btn-outline-light dropdown-item">
-                                Favoritos
-                            </Link>
-                        </li>
-                    </ul>
-                )}
-                {/* Modal para iniciar sesión */}
-                <div
-                    className="modal fade"
-                    id="exampleModal"
-                    tabIndex="-1"
-                    aria-labelledby="exampleModalLabel"
-                    aria-hidden="true"
-                >
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h1 className="modal-title fs-5" id="exampleModalLabel">Iniciar Sesión</h1>
-                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div className="modal-body">
-                                <div className='mx-auto my-auto'>
-                                    <div className="mb-3">
-                                        <label className="form-label">Email</label>
-                                        <input
-                                            type="email"
-                                            className="form-control"
-                                            value={user.email}
-                                            onChange={(event) => setUser({
-                                                ...user,
-                                                email: event.target.value
-                                            })}
-                                            required
-                                        />
-                                    </div>
-                                    <div className="mb-3">
-                                        <label className="form-label">Contraseña</label>
-                                        <div className='d-flex'>
+            <div className="container d-flex justify-content-between">
+
+                <div>
+
+                    <Link to="/" className="navbar-brand text-white">
+                        <img src={logo} alt="Ventu Logo" style={{ width: '120px', height: 'auto' }} />
+                    </Link>
+                    <Link to="/" className="btn">Inicio</Link>
+                    <Link to="/tourplans" className="btn">Planes turisticos</Link>
+                    <Link to="/about" className="btn">Sobre Nosotros</Link>
+                    <Link to="/favorites" className="btn ">Favoritos</Link>
+                </div>
+
+                <div className="dropdown">
+                    <button
+                        className="btn dropdown-toggle"
+                        type="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                    >
+                        {store.user ? `${store.user.username}` : "Iniciar / Registrarse"}
+                    </button>
+                    {!store.token ? (
+                        <ul className="dropdown-menu dropdown-menu-end">
+                            <li>
+                                <button
+                                    className="dropdown-item"
+                                    data-bs-toggle="modal"
+                                    data-bs-target="#exampleModal"
+                                >
+                                    Iniciar sesión
+                                </button>
+                            </li>
+                            <li>
+                                <Link to="/register" className="btn btn-outline-light dropdown-item">
+                                    Registrarse
+                                </Link>
+                            </li>
+                        </ul>
+                    ) : (
+                        <ul className="dropdown-menu">
+                            <li>
+                                <button
+                                    className="dropdown-item"
+                                    onClick={handleLogout}
+                                >
+                                    Cerrar sesión
+                                </button>
+                            </li>
+                            <li>
+                                <Link to="/favorites" className="btn btn-outline-light dropdown-item">
+                                    Favoritos
+                                </Link>
+                            </li>
+                        </ul>
+                    )}
+                    {/* Modal para iniciar sesión */}
+                    <div
+                        className="modal fade"
+                        id="exampleModal"
+                        tabIndex="-1"
+                        aria-labelledby="exampleModalLabel"
+                        aria-hidden="true"
+                    >
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h1 className="modal-title fs-5" id="exampleModalLabel">Iniciar Sesión</h1>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div className="modal-body">
+                                    <div className='mx-auto my-auto'>
+                                        <div className="mb-3">
+                                            <label className="form-label">Email</label>
                                             <input
-                                                type={showPassword ? "text" : "password"}
+                                                type="email"
                                                 className="form-control"
-                                                value={user.password}
+                                                value={user.email}
                                                 onChange={(event) => setUser({
                                                     ...user,
-                                                    password: event.target.value
+                                                    email: event.target.value
                                                 })}
                                                 required
                                             />
-                                            <div
-                                                className='btn'
-                                                onClick={() => setShowPassword(!showPassword)}
-                                            >
-                                                {showPassword ? (
-                                                    <i className="fa-solid fa-lock"></i>
-                                                ) : (
-                                                    <i className="fa-solid fa-eye"></i>
-                                                )}
+                                        </div>
+                                        <div className="mb-3">
+                                            <label className="form-label">Contraseña</label>
+                                            <div className='d-flex'>
+                                                <input
+                                                    type={showPassword ? "text" : "password"}
+                                                    className="form-control"
+                                                    value={user.password}
+                                                    onChange={(event) => setUser({
+                                                        ...user,
+                                                        password: event.target.value
+                                                    })}
+                                                    required
+                                                />
+                                                <div
+                                                    className='btn'
+                                                    onClick={() => setShowPassword(!showPassword)}
+                                                >
+                                                    {showPassword ? (
+                                                        <i className="fa-solid fa-lock"></i>
+                                                    ) : (
+                                                        <i className="fa-solid fa-eye"></i>
+                                                    )}
+                                                </div>
                                             </div>
                                         </div>
+                                        <div className='btn btn-link'>Olvidaste tu contraseña?</div>
                                     </div>
-                                    <div className='btn btn-link'>Olvidaste tu contraseña?</div>
                                 </div>
-                            </div>
-                            <div className="d-flex">
-                                <div className='d-flex flex-column w-100 mx-3 py-2'>
-                                    <button onClick={handleLogin} className="btn btn-success mt-2">
-                                        Iniciar Sesión
-                                    </button>
-                                    <button
-                                        className="btn btn-primary text-center mt-2"
-                                        data-bs-dismiss="modal"
-                                        onClick={() => navigate("/register")}
-                                    >
-                                        Registrarse
-                                    </button>
+                                <div className="d-flex">
+                                    <div className='d-flex flex-column w-100 mx-3 py-2'>
+                                        <button onClick={handleLogin} className="btn btn-success mt-2">
+                                            Iniciar Sesión
+                                        </button>
+                                        <button
+                                            className="btn btn-primary text-center mt-2"
+                                            data-bs-dismiss="modal"
+                                            onClick={() => navigate("/register")}
+                                        >
+                                            Registrarse
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </nav>
     );
 };
