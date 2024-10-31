359a7ae7ae159cbee73773defc51c62604ba6d94
María Fernanda González <mafergonzalezw@gmail.com>
2024-10-31 21:41:40 +0000

    cambio de algunos padding en el home

--- src/front/js/component/MainDestinations.js
+++ src/front/js/component/MainDestinations.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const MainDestinations = ({ destinos }) => {
     return (
-        <section className="destinos-principales py-5">
+        <section className="destinos-principales pb-5 pt-3">
             <div className="container text-center">
-                <h2>Principales destinos en Venezuela</h2>
+                <h2 className="mb-3">Principales destinos en Venezuela</h2>
                 <div className="row">
                     {destinos.map((destino, index) => (
                         <div className="col-6 col-md-4" key={index}>
